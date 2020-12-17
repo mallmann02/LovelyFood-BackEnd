99ae720f4e969ee62c1a7589b3eb5c7c899d0c0a
mallmann02 <leomallmann020302@gmail.com>
2020-12-16 21:52:07 -0300

    Postgress Integration Is Ok

--- knexfile.ts
+++ knexfile.ts
@@ -6,7 +6,8 @@ module.exports = {
         host : 'ec2-54-205-248-255.compute-1.amazonaws.com',
         user : 'sjeujmtugnzbtg',
         password : 'c48393a22783d65723997d5a3e87f11698ad6d435ae774944f2093ef427e56f8',
-        database : 'd4ckflog1n3q2n'
+        database : 'd4ckflog1n3q2n',
+        ssl: {rejectUnauthorized: false}
     },
 
     migrations:{
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,5 +1,4 @@
 import knex from 'knex';
-import path from 'path';
 
 const connection = knex({
     client: 'pg',
@@ -7,7 +6,8 @@ const connection = knex({
         host : 'ec2-54-205-248-255.compute-1.amazonaws.com',
         user : 'sjeujmtugnzbtg',
         password : 'c48393a22783d65723997d5a3e87f11698ad6d435ae774944f2093ef427e56f8',
-        database : 'd4ckflog1n3q2n'
+        database : 'd4ckflog1n3q2n',
+        ssl: {rejectUnauthorized: false}
     },
 
     pool: {min:0, max:20},
