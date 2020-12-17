0067d4dd52b779e1110e1c06c68fd024b5de5fa2
mallmann02 <leomallmann020302@gmail.com>
2020-12-16 22:51:52 -0300

    Removing Files With Private Information

--- src/database/seeds/01_create_administrators.ts
+++
@@ -1,9 +0,0 @@
-import Knex from 'knex';
-
-export async function seed(knex: Knex) {
-    await knex('administrators').insert([
-        {email: 'mallmann2002@hotmail.com', password: 'mallmann_admin', name: 'Leonardo'},
-        {email: 'gillete@hotmail.com', password: 'gillete_admin', name: 'Guilherme'},
-        {email: 'maedogilete@hotmail.com', password: 'maedogilete_admin', name: 'Sandra'},
-    ])
-}
