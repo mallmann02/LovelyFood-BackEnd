3bb27e74b15b2bb94af06967e43551d117ac5589
mallmann02 <leomallmann020302@gmail.com>
2020-12-16 22:55:29 -0300

    Adding Files To .gitignore

--- knexfile.ts
+++ knexfile.ts
@@ -3,10 +3,10 @@ import path from 'path';
 module.exports = {
     client: 'pg',
     connection: {
-        host : 'ec2-54-205-248-255.compute-1.amazonaws.com',
-        user : 'sjeujmtugnzbtg',
-        password : 'c48393a22783d65723997d5a3e87f11698ad6d435ae774944f2093ef427e56f8',
-        database : 'd4ckflog1n3q2n',
+        host : process.env.POSTGRESS_HOST,
+        user : process.env.POSTGRESS_USER,
+        password : process.env.POSTGRESS_PASSWORD,
+        database : process.env.POSTGRESS_DB,
         ssl: {rejectUnauthorized: false}
     },
 
